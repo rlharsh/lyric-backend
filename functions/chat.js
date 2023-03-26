1012937008408df338e0055c79e9731abe1a80ae
Ronald Lee Harsh <ronald.harsh@protonmail.com>
2023-03-25 21:30:30 -0500

    Update for Netlify

---
+++ functions/chat.js
@@ -0,0 +1,32 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const messages = JSON.parse(event.body).messages;
+
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data.choices[0].message)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error processing ChatGPT API request' })
+    };
+  }
+};
